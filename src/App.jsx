fcbbc4d07c37f3e97d5e0ee91f66d52126fb363c
Elfrunner28 <kartispotify@gmail.com>
2025-11-08 17:11:26 -0600

    implemented basic map

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useRef, useEffect } from "react";
+
+function Map() {
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    if (mapRef.current) return;
+
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiYm9ta2EiLCJhIjoiY21ocXZuendlMTB1MTJqcHpqamx3NzVrciJ9.LnpWZyAtsFMkWzRbOyMkKw";
+
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/bomka/cmhqv52hy005d01r0h3udd541",
+      center: [-90.049, 35.146],
+      zoom: 10,
+    });
+
+    mapRef.current.on("style.load", () => {
+      console.log("Map style loaded successfully!");
+    });
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <div ref={mapContainerRef} style={{ width: "100%", height: "100vh" }} />
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>hi</p>
+      <Map />
     </div>
   );
 }
